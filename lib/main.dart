f4cec3b28bb30de2f82da9c79b9a4b8f572f8241
junnelcoder <balasabasjunnel0@gmail.com>
2024-02-23 10:51:27 +0800

    justin problemado

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'pages/viewOrder.dart';
+import 'splashScreen.dart';
 void main() => runApp(
       const MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: HomePage(),
+        home: SplashScreen(),
       ),
     );
 
